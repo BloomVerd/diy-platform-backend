f157faf2eb58bfe18a4afe8b9d2c525db6297882
Del-Ali <delaliagbesidorwu@gmail.com>
2026-01-19 01:41:44 +0000

    initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        process.env.STAGE === 'development'
+          ? `.env.${process.env.STAGE}.local`
+          : '.env',
+      ],
+    }),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      // sortSchema: true,
+      playground: true,
+      introspection: true,
+      resolvers: {},
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      context: ({ req, res }: any) => ({ req, res }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/database/database.providers.ts
@@ -0,0 +1,46 @@
+import { BullModule } from '@nestjs/bullmq';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { DatabaseQueryLogger } from '../shared/interceptors/database-query.logger';
+
+/**
+ * Setup default connection in the application
+ * @param config {ConfigService}
+ */
+const defaultPostgresDBConnection = (
+  configService: ConfigService,
+  //   queryLogger: DatabaseQueryLogger,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  autoLoadEntities: true,
+  synchronize: configService.get('NODE_ENV') !== 'production',
+  url: configService.get('DATABASE_URL'),
+  entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/**/*{.js,.ts}'],
+  migrationsRun: false,
+  ssl: {
+    rejectUnauthorized: false, // allow self-signed AWS certs
+  },
+  logging: true,
+  //   logger: queryLogger,
+  maxQueryExecutionTime: 1000, // Log queries slower than 1 second
+});
+
+const defaultRedisDBConnection = async (configService: ConfigService) => ({
+  connection: {
+    url: configService.get<string>('REDIS_URL'),
+  },
+});
+
+export const databaseProviders = [
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: defaultPostgresDBConnection,
+  }),
+  BullModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: defaultRedisDBConnection,
+  }),
+];
